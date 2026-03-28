db4faeb6c3955547840c6bba3913c9ac1eb273b4
momo-siu <13530305314@163.com>
2026-03-28 10:20:33 +0800

    给小东子的，寻路逻辑不改就不用更新

---
+++ build/RVO2/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/19255/Desktop/dachuang/rvo/cpp-rvo-simulator/RVO2
+# Build directory: C:/Users/19255/Desktop/dachuang/build/RVO2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("examples")
+subdirs("doc")
